c9f5123afb481d2b5165910d0dc2d608fd648836
jkochavi <jdkochavi@comcast.net>
2021-09-24 14:05:46 -0700

    first commit

---
+++ src/interface.cpp
@@ -0,0 +1,121 @@
+#include "lvgl.h"
+#include "interface.h"
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+static lv_obj_t * scr;                  // Screen object
+static lv_obj_t * coms_button;          // Communications button object
+static lv_obj_t * settings_button;      // Settings button object
+static lv_obj_t * telemetry_button;     // Telemetry button object
+static lv_obj_t * sensors_button;       // Sensors button object
+static lv_style_t gold_button_style;
+static lv_style_t gold_button_pressed_style;
+static lv_style_t pink1_button_style;
+static lv_style_t pink1_button_pressed_style;
+
+void add_gold_button(lv_obj_t * btn, char * label_text, int x, int y)
+{
+    lv_style_init(&gold_button_style); 
+    lv_style_set_radius(&gold_button_style, 30);
+    lv_style_set_bg_opa(&gold_button_style, LV_OPA_100); 
+    lv_style_set_bg_color(&gold_button_style, lv_color_LCARS_gold()); 
+    lv_style_set_text_color(&gold_button_style, lv_color_black()); 
+    lv_style_set_pad_all(&gold_button_style, 10); 
+    lv_style_init(&gold_button_pressed_style); 
+    lv_style_set_outline_width(&gold_button_pressed_style, 30); 
+    lv_style_set_outline_opa(&gold_button_pressed_style, LV_OPA_TRANSP); 
+    lv_style_set_translate_y(&gold_button_pressed_style, 5); 
+    lv_style_set_bg_color(&gold_button_pressed_style, lv_color_darken(lv_color_LCARS_gold(), 127)); 
+    static lv_style_transition_dsc_t trans;
+    static lv_style_prop_t props[] = {LV_STYLE_OUTLINE_WIDTH, LV_STYLE_OUTLINE_OPA, (lv_style_prop_t)0};
+    lv_style_transition_dsc_init(&trans, props, lv_anim_path_linear, 300, 0, NULL);
+    lv_style_set_transition(&gold_button_pressed_style, &trans); 
+    lv_obj_remove_style_all(btn);   
+    lv_obj_add_style(btn, &gold_button_style, LV_STATE_DEFAULT);
+    lv_obj_add_style(btn, &gold_button_pressed_style, LV_STATE_PRESSED);
+    lv_obj_set_size(btn, 100, 40);
+    lv_obj_set_x(btn, x);
+    lv_obj_set_y(btn, y);
+    lv_obj_t * label = lv_label_create(btn);
+    lv_label_set_text(label, label_text);
+    lv_obj_center(label);
+}
+
+void add_pink1_button(lv_obj_t * btn, char * label_text, int x, int y)
+{
+    lv_style_init(&pink1_button_style); 
+    lv_style_set_radius(&pink1_button_style, 30);
+    lv_style_set_bg_opa(&pink1_button_style, LV_OPA_100); 
+    lv_style_set_bg_color(&pink1_button_style, lv_color_LCARS_pink_var1()); 
+    lv_style_set_text_color(&pink1_button_style, lv_color_black()); 
+    lv_style_set_pad_all(&pink1_button_style, 10); 
+    lv_style_init(&pink1_button_pressed_style); 
+    lv_style_set_outline_width(&pink1_button_pressed_style, 30); 
+    lv_style_set_outline_opa(&pink1_button_pressed_style, LV_OPA_TRANSP); 
+    lv_style_set_translate_y(&pink1_button_pressed_style, 5); 
+    lv_style_set_bg_color(&pink1_button_pressed_style, lv_color_darken(lv_color_LCARS_pink_var1(), 127)); 
+    static lv_style_transition_dsc_t trans;
+    static lv_style_prop_t props[] = {LV_STYLE_OUTLINE_WIDTH, LV_STYLE_OUTLINE_OPA, (lv_style_prop_t)0};
+    lv_style_transition_dsc_init(&trans, props, lv_anim_path_linear, 300, 0, NULL);
+    lv_style_set_transition(&pink1_button_pressed_style, &trans); 
+    lv_obj_remove_style_all(btn);   
+    lv_obj_add_style(btn, &pink1_button_style, LV_STATE_DEFAULT);
+    lv_obj_add_style(btn, &pink1_button_pressed_style, LV_STATE_PRESSED);
+    lv_obj_set_size(btn, 100, 40);
+    lv_obj_set_x(btn, x);
+    lv_obj_set_y(btn, y);
+    lv_obj_t * label = lv_label_create(btn);
+    lv_label_set_text(label, label_text);
+    lv_obj_center(label);
+}
+
+
+void add_button(lv_obj_t * btn)
+{
+    /*Halo on press*/
+    static lv_style_t style_halo;
+    lv_style_init(&style_halo);
+    lv_style_set_transition_time(&style_halo, LV_STATE_PRESSED, 400);
+    lv_style_set_transition_time(&style_halo, LV_STATE_DEFAULT, 0);
+    lv_style_set_transition_delay(&style_halo, LV_STATE_DEFAULT, 200);
+    lv_style_set_outline_width(&style_halo, LV_STATE_DEFAULT, 0);
+    lv_style_set_outline_width(&style_halo, LV_STATE_PRESSED, 20);
+    lv_style_set_outline_opa(&style_halo, LV_STATE_DEFAULT, LV_OPA_COVER);
+    lv_style_set_outline_opa(&style_halo, LV_STATE_FOCUSED, LV_OPA_COVER);   /*Just to be sure, the theme might use it*/
+    lv_style_set_outline_opa(&style_halo, LV_STATE_PRESSED, LV_OPA_TRANSP);
+    lv_style_set_transition_prop_1(&style_halo, LV_STATE_DEFAULT, LV_STYLE_OUTLINE_OPA);
+    lv_style_set_transition_prop_2(&style_halo, LV_STATE_DEFAULT, LV_STYLE_OUTLINE_WIDTH);
+
+    lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_add_style(btn2, LV_BTN_PART_MAIN, &style_halo);
+    lv_obj_set_style_local_value_str(btn2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, "Halo");
+}
+
+void LCARS_bg(lv_obj_t * scr)
+{
+    LV_IMG_DECLARE(homepage);
+    lv_obj_t * img1 = lv_img_create(scr);
+    lv_img_set_src(img1, &homepage);
+    lv_obj_set_size(img1, 320, 240);
+}
+
+
+void interface(void)
+{
+    scr = lv_obj_create(NULL);
+    LCARS_bg(scr);
+    lv_scr_load_anim(scr, LV_SCR_LOAD_ANIM_MOVE_TOP, 1000, 0, false);
+    
+    coms_button = lv_btn_create(scr);
+    settings_button = lv_btn_create(scr);
+    telemetry_button = lv_btn_create(scr);
+    sensors_button = lv_btn_create(scr);
+    
+    add_pink1_button(coms_button, "COMS", 195, 25);
+    add_pink1_button(settings_button, "SYSTEM", 195, 75);
+    add_gold_button(telemetry_button, "TELEM", 195, 125);
+    add_gold_button(sensors_button, "SCAN", 195, 175);
+    
+}
---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+#include "Arduino.h"
+#include "TFT_eSPI.h"
+#include "lvgl.h"
+#include "interface.h"
+
+
+TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
+
+/*Change to your screen resolution*/
+static const uint32_t screenWidth  = 320;
+static const uint32_t screenHeight = 240;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[ screenWidth * 10 ];
+
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+   uint32_t w = (area->x2 - area->x1 + 1);
+   uint32_t h = (area->y2 - area->y1 + 1);
+
+   tft.startWrite();
+   tft.setAddrWindow( area->x1, area->y1, w, h );
+   tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+   tft.endWrite();
+
+   lv_disp_flush_ready( disp );
+}
+
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
+{
+   uint16_t touchX, touchY;
+   bool touched = tft.getTouch( &touchX, &touchY, 600 );
+   if( !touched )
+   {
+      data->state = LV_INDEV_STATE_REL;
+   }
+   else
+   {
+      data->state = LV_INDEV_STATE_PR;
+      /*Set the coordinates*/
+      data->point.x = touchX;
+      data->point.y = touchY;
+   }
+}
+
+void setup()
+{
+   lv_init();
+   tft.begin();          /* TFT init */
+   tft.setRotation(3); /* Landscape orientation, flipped */
+   /*Set the touchscreen calibration data,
+     the actual data for your display can be aquired using
+     the Generic -> Touch_calibrate example from the TFT_eSPI library*/
+   uint16_t calData[5] = { 275, 3620, 264, 3532, 1 };
+   tft.setTouch(calData);
+   lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * 10 );
+   /*Initialize the display*/
+   static lv_disp_drv_t disp_drv;
+   lv_disp_drv_init( &disp_drv );
+   /*Change the following line to your display resolution*/
+   disp_drv.hor_res = screenWidth;
+   disp_drv.ver_res = screenHeight;
+   disp_drv.flush_cb = my_disp_flush;
+   disp_drv.draw_buf = &draw_buf;
+   lv_disp_drv_register(&disp_drv);
+   /*Initialize the (dummy) input device driver*/
+   static lv_indev_drv_t indev_drv;
+   lv_indev_drv_init( &indev_drv );
+   indev_drv.type = LV_INDEV_TYPE_POINTER;
+   indev_drv.read_cb = my_touchpad_read;
+   lv_indev_drv_register( &indev_drv );
+   //----------Custom code begins here------------//
+   interface();
+
+
+}
+
+void loop()
+{
+   lv_timer_handler(); /* let the GUI do its work */
+   delay(5);
+}
+
