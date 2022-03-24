4d19f650ac6144216cf82b33b5aa0a52212d3c54
jkochavi <jdkochavi@comcast.net>
2022-03-24 11:59:50 -0700

    Working on comments

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
    uint32_t h = (area->y2 - area->y1 + 1);
    tft.startWrite();
    tft.setAddrWindow( area->x1, area->y1, w, h );
-   tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+   tft.pushColors((uint16_t*)&color_p->full, w * h, true);
    tft.endWrite();
    lv_disp_flush_ready( disp );
 }
@@ -24,14 +24,10 @@ void my_touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data)
 {
    uint16_t touchX, touchY;
    bool touched = tft.getTouch( &touchX, &touchY, 600 );
-   if( !touched )
-   {
-      data->state = LV_INDEV_STATE_REL;
-   }
+   if(!touched){data->state = LV_INDEV_STATE_REL;}
    else
    {
       data->state = LV_INDEV_STATE_PR;
-      /*Set the coordinates*/
       data->point.x = touchX;
       data->point.y = touchY;
    }
@@ -40,24 +36,18 @@ void my_touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data)
 void setup()
 {
    lv_init();
-   tft.begin();          /* TFT init */
-   tft.setRotation(3); /* Landscape orientation, flipped */
-   /*Set the touchscreen calibration data,
-     the actual data for your display can be aquired using
-     the Generic -> Touch_calibrate example from the TFT_eSPI library*/
+   tft.begin();         
+   tft.setRotation(3); 
    uint16_t calData[5] = { 275, 3620, 264, 3532, 1 };
    tft.setTouch(calData);
    lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * 10 );
-   /*Initialize the display*/
    static lv_disp_drv_t disp_drv;
    lv_disp_drv_init( &disp_drv );
-   /*Change the following line to your display resolution*/
    disp_drv.hor_res = screenWidth;
    disp_drv.ver_res = screenHeight;
    disp_drv.flush_cb = my_disp_flush;
    disp_drv.draw_buf = &draw_buf;
    lv_disp_drv_register(&disp_drv);
-   /*Initialize the (dummy) input device driver*/
    static lv_indev_drv_t indev_drv;
    lv_indev_drv_init( &indev_drv );
    indev_drv.type = LV_INDEV_TYPE_POINTER;
@@ -69,7 +59,7 @@ void setup()
 
 void loop()
 {
-   lv_timer_handler(); /* let the GUI do its work */
+   lv_timer_handler();
    delay(5);
 }
 
