59a35089ce912be3e5af6da867468229d2bd259b
jkochavi <jdkochavi@comcast.net>
2022-03-23 19:46:23 -0700

    Fixing comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,24 @@
 #include "lvgl.h"
 #include "GUI/ui.h"
 
-TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
-
-/*Change to your screen resolution*/
+TFT_eSPI tft = TFT_eSPI(); 
 static const uint32_t screenWidth  = 320;
 static const uint32_t screenHeight = 240;
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * 10 ];
 
-/* Display flushing */
-void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
    uint32_t w = (area->x2 - area->x1 + 1);
    uint32_t h = (area->y2 - area->y1 + 1);
-
    tft.startWrite();
    tft.setAddrWindow( area->x1, area->y1, w, h );
    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
    tft.endWrite();
-
    lv_disp_flush_ready( disp );
 }
 
-/*Read the touchpad*/
-void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
+void my_touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data)
 {
    uint16_t touchX, touchY;
    bool touched = tft.getTouch( &touchX, &touchY, 600 );
