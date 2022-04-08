358db64ff547a0c0d71c8123e395998c76f74a16
jkochavi <54564260+jkochavi@users.noreply.github.com>
2022-04-07 22:06:06 -0700

    Added SquareLine files

---
+++ SquareLine/Tricorder2_events.py
@@ -0,0 +1,20 @@
+
+def wificonnect(event_struct):
+    return
+
+
+def bluetoothconnect(event_struct):
+    return
+
+
+def resizeOptionsPanel(event_struct):
+    return
+
+
+def OptionsPanelSmall(event_struct):
+    return
+
+
+def OptionsPanelBig(event_struct):
+    return
+
