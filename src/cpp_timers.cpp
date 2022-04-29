0e12a2968de89470dd21f85475a5ae43ed737e43
jkochavi <54564260+jkochavi@users.noreply.github.com>
2022-04-28 19:04:05 -0700

    Modified battery level

--- src/cpp_timers.cpp
+++ src/cpp_timers.cpp
@@ -6,10 +6,13 @@
 
 float batterylevel()
 {
-    float battery_level = analogRead(batteryPin); // Raw ADC reading
-    battery_level *= 3.3/4096;                    // Convert to voltage
-    battery_level = 166.67*battery_level-250;     // Convert to percentage
-    if (battery_level > 100) {battery_level=100;} // Saturate
+    float battery_level = analogRead(batteryPin);         // Raw ADC reading
+    lv_textarea_add_text(ui_console, 
+        ((String)battery_level).c_str());
+    lv_textarea_add_text(ui_console,"\n");
+    battery_level *= 3.3/4096;                            // Convert to voltage
+    battery_level = 166.67*battery_level-250;             // Convert to percentage
+    if (battery_level > 100) {battery_level=100;}         // Saturate
     return battery_level;
 }
 
@@ -17,7 +20,6 @@ void batterytimer(lv_timer_t * timer)
 {
     float battery_level = batterylevel();
     lv_label_set_text_fmt(ui_battery, "BATT %d%s", (int)battery_level, "%");
-    lv_textarea_add_text(ui_console, "BATT CHECK\n");
 }
 
 void console_timer(lv_timer_t * timer)
