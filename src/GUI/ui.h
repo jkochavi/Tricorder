4babc1b589430a1c397f74cdc61b6d3ef9bf035b
jkochavi <54564260+jkochavi@users.noreply.github.com>
2022-06-17 15:55:01 -0700

    Added sensor readings to home page

--- src/GUI/ui.h
+++ src/GUI/ui.h
@@ -26,6 +26,8 @@ extern lv_obj_t * ui_clear;
 extern lv_obj_t * ui_logswitch;
 extern lv_obj_t * ui_character;
 extern lv_obj_t * ui_keyboard;
+extern lv_obj_t * ui_sensorpack1;
+extern lv_obj_t * ui_sensorreadings1;
 extern lv_obj_t * ui_LCARS;
 extern lv_obj_t * ui_back1;
 
@@ -37,6 +39,7 @@ LV_IMG_DECLARE(ui_img_home_png);    // assets\home.png
 LV_IMG_DECLARE(ui_img_halfbutton5_png);    // assets\halfbutton5.png
 LV_IMG_DECLARE(ui_img_halfbutton2_png);    // assets\halfbutton2.png
 LV_IMG_DECLARE(ui_img_halfbutton4_png);    // assets\halfbutton4.png
+LV_IMG_DECLARE(ui_img_sensorpack1_png);    // assets\sensorpack1.png
 LV_IMG_DECLARE(ui_img_lcars_png);    // assets\LCARS.png
 
 
