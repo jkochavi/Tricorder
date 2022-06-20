819d4a4c622908f3812d895f2e832e4efa05a385
jkochavi <54564260+jkochavi@users.noreply.github.com>
2022-06-19 21:56:32 -0700

    Removed macro padd

--- src/GUI/ui.h
+++ src/GUI/ui.h
@@ -22,7 +22,6 @@ extern lv_obj_t * ui_keyboardbutton;
 extern lv_obj_t * ui_filebutton;
 extern lv_obj_t * ui_console;
 extern lv_obj_t * ui_clear;
-extern lv_obj_t * ui_macrobutton;
 extern lv_obj_t * ui_sigchoice;
 extern lv_obj_t * ui_character;
 extern lv_obj_t * ui_keyboard;
@@ -30,23 +29,8 @@ extern lv_obj_t * ui_LCARS;
 extern lv_obj_t * ui_back1;
 extern lv_obj_t * ui_cmdbutton;
 extern lv_obj_t * ui_sdcontents;
-extern lv_obj_t * ui_macroconfig;
 extern lv_obj_t * ui_cmdtextarea;
 extern lv_obj_t * ui_cmdkeyboard;
-extern lv_obj_t * ui_Macro;
-extern lv_obj_t * ui_b1;
-extern lv_obj_t * ui_b2;
-extern lv_obj_t * ui_b3;
-extern lv_obj_t * ui_b4;
-extern lv_obj_t * ui_b5;
-extern lv_obj_t * ui_b6;
-extern lv_obj_t * ui_b7;
-extern lv_obj_t * ui_b8;
-extern lv_obj_t * ui_b9;
-extern lv_obj_t * ui_back2;
-extern lv_obj_t * ui_row1;
-extern lv_obj_t * ui_row2;
-extern lv_obj_t * ui_row3;
 
 void clearconsoletext(lv_event_t * e);
 void keypress(lv_event_t * e);
@@ -56,10 +40,8 @@ LV_IMG_DECLARE(ui_img_home_png);    // assets\home.png
 LV_IMG_DECLARE(ui_img_halfbutton5_png);    // assets\halfbutton5.png
 LV_IMG_DECLARE(ui_img_halfbutton2_png);    // assets\halfbutton2.png
 LV_IMG_DECLARE(ui_img_halfbutton4_png);    // assets\halfbutton4.png
-LV_IMG_DECLARE(ui_img_halfbutton3_png);    // assets\halfbutton3.png
 LV_IMG_DECLARE(ui_img_lcars_png);    // assets\LCARS.png
 LV_IMG_DECLARE(ui_img_halfbutton1_png);    // assets\halfbutton1.png
-LV_IMG_DECLARE(ui_img_macro_png);    // assets\macro.png
 
 
 LV_FONT_DECLARE(ui_font_Regular_Text);
